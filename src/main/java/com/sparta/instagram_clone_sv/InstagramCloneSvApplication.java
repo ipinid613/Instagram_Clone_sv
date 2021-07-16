70f38cbbde796183d4e1b676dbce70ade52d417d
ipinid613 <ipinid613@kakao.com>
2021-07-16 23:06:26 +0900

    Timestamped 작동 안 하는 오류 해결

--- src/main/java/com/sparta/instagram_clone_sv/InstagramCloneSvApplication.java
+++ src/main/java/com/sparta/instagram_clone_sv/InstagramCloneSvApplication.java
@@ -2,10 +2,12 @@ package com.sparta.instagram_clone_sv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class InstagramCloneSvApplication {
 
